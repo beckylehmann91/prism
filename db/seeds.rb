2309d6fcbe5139ba6676bd9db0e3431d60fd488f
Brian Wagner <briwagner@outlook.com>
2016-01-31 16:32:07 -0600

    Add brian sound files- percus100 and pad100;  and seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Sound.create(filename: "/sounds/Strings_1_AMi_100.mp3", luminence: 2, color_domi
 Sound.create(filename: "/sounds/Strings_2_GMa_100.mp3", luminence: 2, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 3
-Sound.create(filename: "/sounds/Contemplative_Synth_1_AMi_100.mp3", luminence: 3, color_dominance: 1, role: "melody")
+# Sound.create(filename: "/sounds/Contemplative_Synth_1_AMi_100.mp3", luminence: 3, color_dominance: 1, role: "melody")
 # Borealis isn't really a synth, but it's a great soundtrack! Very blue.
 Sound.create(filename: "/sounds/Borealis_AMi_100.mp3", luminence: 3, color_dominance: 1, role: "melody")
 Sound.create(filename: "/sounds/Synth_2_GMa_100.mp3", luminence: 3, color_dominance: 2, role: "melody")
@@ -27,6 +27,17 @@ Sound.create(filename: "/sounds/Violin_1_AMi_100.mp3", luminence: 7, color_domin
 Sound.create(filename: "/sounds/Violin_1_GMa_100.mp3", luminence: 7, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 8
+Sound.create(filename: "/sounds/melody100_bass.mp3", luminence: 1, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_cello.mp3", luminence: 2, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_synth.mp3", luminence: 3, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_tenor.mp3", luminence: 4, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_guitar.mp3", luminence: 5, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_piano.mp3", luminence: 6, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_violin", luminence: 7, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_trumpet", luminence: 8, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_flute.mp3", luminence: 9, color_dominance: 3, role: "melody")
+Sound.create(filename: "/sounds/melody100_harp.mp3", luminence: 10, color_dominance: 3, role: "melody")
+
 
 # MELODY: LUMINENCE 9
 # MELODY: LUMINENCE 10
@@ -41,4 +52,25 @@ Sound.create(filename: "/sounds/Violin_1_GMa_100.mp3", luminence: 7, color_domin
 # Sound.create(filename: "/sounds/Strings_6_CMa_100.mp3", luminence: 2, color: 7, contrast: 1)
 
 # Pads
-Sounds.create(filename: "/sounds/", role: "pad", luminence: )
+Sound.create(filename: "/sounds/pad100-001.mp3", role: "pad", color_variety: 1)
+Sound.create(filename: "/sounds/pad100-002.mp3", role: "pad", color_variety: 2)
+Sound.create(filename: "/sounds/pad100-003.mp3", role: "pad", color_variety: 3)
+Sound.create(filename: "/sounds/pad100-004.mp3", role: "pad", color_variety: 4)
+Sound.create(filename: "/sounds/pad100-005.mp3", role: "pad", color_variety: 5)
+Sound.create(filename: "/sounds/pad100-006.mp3", role: "pad", color_variety: 6)
+Sound.create(filename: "/sounds/pad100-007.mp3", role: "pad", color_variety: 7)
+Sound.create(filename: "/sounds/pad100-008.mp3", role: "pad", color_variety: 8)
+Sound.create(filename: "/sounds/pad100-009.mp3", role: "pad", color_variety: 9)
+Sound.create(filename: "/sounds/pad100-0010.mp3", role: "pad", color_variety: 10)
+
+# Percussion
+Sound.create(filename: "/sounds/perc100-001.mp3", role: "percussion", contrast: 1)
+Sound.create(filename: "/sounds/perc100-002.mp3", role: "percussion", contrast: 2)
+Sound.create(filename: "/sounds/perc100-003.mp3", role: "percussion", contrast: 3)
+Sound.create(filename: "/sounds/perc100-004.mp3", role: "percussion", contrast: 4)
+Sound.create(filename: "/sounds/perc100-005.mp3", role: "percussion", contrast: 5)
+Sound.create(filename: "/sounds/perc100-006.mp3", role: "percussion", contrast: 6)
+Sound.create(filename: "/sounds/perc100-007.mp3", role: "percussion", contrast: 7)
+Sound.create(filename: "/sounds/perc100-008.mp3", role: "percussion", contrast: 8)
+Sound.create(filename: "/sounds/perc100-009.mp3", role: "percussion", contrast: 9)
+Sound.create(filename: "/sounds/perc100-0010.mp3", role: "percussion", contrast: 10)
