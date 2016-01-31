6f1301f89d274f09e200c41ba84807dfc38ac4ff
Brian Wagner <briwagner@outlook.com>
2016-01-31 16:00:29 -0600

    Change sound table cols; color becomes color_dominance; add color_variety in table; edit seed to match

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
 # MELODY: LUMINENCE 1
-Sound.create(filename: "/sounds/Bass_1_AMi_100.mp3", luminence: 1, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Bass_2_GMa_100.mp3", luminence: 1, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Bass_1_AMi_100.mp3", luminence: 1, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Bass_2_GMa_100.mp3", luminence: 1, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 2
-Sound.create(filename: "/sounds/Strings_1_AMi_100.mp3", luminence: 2, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Strings_2_GMa_100.mp3", luminence: 2, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Strings_1_AMi_100.mp3", luminence: 2, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Strings_2_GMa_100.mp3", luminence: 2, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 3
-Sound.create(filename: "/sounds/Contemplative_Synth_1_AMi_100.mp3", luminence: 3, color: 1, role: "melody")
+Sound.create(filename: "/sounds/Contemplative_Synth_1_AMi_100.mp3", luminence: 3, color_dominance: 1, role: "melody")
 # Borealis isn't really a synth, but it's a great soundtrack! Very blue.
-Sound.create(filename: "/sounds/Borealis_AMi_100.mp3", luminence: 3, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Synth_2_GMa_100.mp3", luminence: 3, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Borealis_AMi_100.mp3", luminence: 3, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Synth_2_GMa_100.mp3", luminence: 3, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 4
 
 # MELODY: LUMINENCE 5
-Sound.create(filename: "/sounds/Acoustic_Guitar_1_AMi_100.mp3", luminence: 5, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Acoustic_Guitar_2_GMa_100.mp3", luminence: 5, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Acoustic_Guitar_1_AMi_100.mp3", luminence: 5, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Acoustic_Guitar_2_GMa_100.mp3", luminence: 5, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 6
-Sound.create(filename: "/sounds/Delicate_Piano_4_AMi_100.mp3", luminence: 6, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Delicate_Piano_1_GMa_100.mp3", luminence: 6, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Delicate_Piano_4_AMi_100.mp3", luminence: 6, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Delicate_Piano_1_GMa_100.mp3", luminence: 6, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 7
-Sound.create(filename: "/sounds/Violin_1_AMi_100.mp3", luminence: 7, color: 1, role: "melody")
-Sound.create(filename: "/sounds/Violin_1_GMa_100.mp3", luminence: 7, color: 2, role: "melody")
+Sound.create(filename: "/sounds/Violin_1_AMi_100.mp3", luminence: 7, color_dominance: 1, role: "melody")
+Sound.create(filename: "/sounds/Violin_1_GMa_100.mp3", luminence: 7, color_dominance: 2, role: "melody")
 
 # MELODY: LUMINENCE 8
 
@@ -39,3 +39,6 @@ Sound.create(filename: "/sounds/Violin_1_GMa_100.mp3", luminence: 7, color: 2, r
 # Sound.create(filename: "/sounds/Delicate_Piano_1_CMa_100.mp3", luminence: 6, color: 4, contrast: 1)
 # Sound.create(filename: "/sounds/Piano_CMa_100.mp3", luminence: 6, contrast: 1)
 # Sound.create(filename: "/sounds/Strings_6_CMa_100.mp3", luminence: 2, color: 7, contrast: 1)
+
+# Pads
+Sounds.create(filename: "/sounds/", role: "pad", luminence: )
