f274e9896346f4f1001a33abedf81ff0f21aaacd
Pratik Shah <pratik.t.shah@gmail.com>
2016-01-31 22:53:27 -0600

    carousel but no spacing

--- app/assets/javascripts/carousel_configs.js
+++ app/assets/javascripts/carousel_configs.js
@@ -1,12 +1,14 @@
-$(function(){
+$(document).ready(function(){
   var owl = $("#img-carousel");
 
   owl.owlCarousel({
     autoPlay: true,
     stopOnHover: true,
-    items: 10,                  // 10 imgs over 1000px browser
-    itemsDesktop: [1000, 5]     // 5 imgs between 1000px and 901px
-    itemsDesktopSmall: [900, 3] // 3 imgs 900px and below
+    items: 7,                   // 7 imgs over 1000px browser
+    itemsDesktop: [1000, 5],    // 5 imgs between 1000px and 901px
+    itemsDesktopSmall: [900, 3],// 3 imgs 900px and below
+    itemsTablet: [600, 2],      // 2 items between 600 and 0
+    itemsMobile: false          // inherit from tablet
   });
 
 });
