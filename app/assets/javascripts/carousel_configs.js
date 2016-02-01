6ad8ff8f0ea1b85dda97bd3d0958a423bf537fb4
Brian Wagner <email": null>
2016-02-01 10:54:53 -0600

    Fix breakpoints on carousel; upper limit is 5 images

--- app/assets/javascripts/carousel_configs.js
+++ app/assets/javascripts/carousel_configs.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     autoPlay: true,
     stopOnHover: true,
     items: 5,                   // 7 imgs over 1000px browser
-    itemsDesktop: [1600, 5],    // 5 imgs between 1000px and 901px
-    itemsDesktopSmall: [1599, 4],// 3 imgs 900px and below
-    itemsTablet: [800, 3],      // 2 items between 600 and 0
-    itemsTabletSmall: [600, 2],
+    itemsDesktop: [1400, 5],    // 5 imgs between 1000px and 901px
+    itemsDesktopSmall: [1399, 4],// 3 imgs 900px and below
+    itemsTablet: [900, 3],      // 2 items between 600 and 0
+    itemsTabletSmall: [899, 2],
     itemsMobile: [599, 1]          // inherit from tablet
   });
 
