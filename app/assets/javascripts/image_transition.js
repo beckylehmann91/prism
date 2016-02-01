9b6ba74720540bbcf61f5dfb7f6cad36f5dd95a2
beckylehmann91 <beckylehmann91@gmail.com>
2016-02-01 16:25:50 -0600

    Create background transition for home page

---
+++ app/assets/javascripts/image_transition.js
@@ -0,0 +1,16 @@
+$(function() {
+  setInterval("nextImage()", 7000);
+});
+
+function nextImage() {
+  var oCurPhoto = $('#photoShow div.current');  // current pic with z-index 2
+  var oNxtPhoto = oCurPhoto.next();       // select next div element with pic
+  if (oNxtPhoto.length == 0)
+        oNxtPhoto = $('#photoShow div:first');  // cycle back to first image
+
+  oCurPhoto.removeClass('current').addClass('previous');  //current photo remove class add previous
+  oNxtPhoto.css('opacity', '0.0').addClass('current').animate( { 'opacity': '1.0' }, 1000,
+    function() {
+      oCurPhoto.removeClass('previous');
+    });
+}
