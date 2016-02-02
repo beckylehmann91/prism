d0ac0c4bebfcb8ad39a5999c2225e04f2f407dab
Brian Wagner <email": null>
2016-02-02 09:41:47 -0600

    Unified DB to accomodate calls to AWS; change post controller and view to match

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = [Post.find(20)]
+    @posts = Post.first(7)
   end
 
   def show
