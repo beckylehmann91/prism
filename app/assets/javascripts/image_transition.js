0453f7fdcb6b330a3b279b51da96797e75ca10a6
beckylehmann91 <beckylehmann91@gmail.com>
2016-02-02 11:30:56 -0600

    Update posts styling

--- app/assets/javascripts/image_transition.js
+++ app/assets/javascripts/image_transition.js
@@ -3,10 +3,10 @@ $(function() {
 });
 
 function nextImage() {
-  var oCurPhoto = $('#photoShow div.current');  // current pic with z-index 2
+  var oCurPhoto = $('.photoShow div.current');  // current pic with z-index 2
   var oNxtPhoto = oCurPhoto.next();       // select next div element with pic
   if (oNxtPhoto.length == 0)
-        oNxtPhoto = $('#photoShow div:first');  // cycle back to first image
+        oNxtPhoto = $('.photoShow div:first');  // cycle back to first image
 
   oCurPhoto.removeClass('current').addClass('previous');  //current photo remove class add previous
   oNxtPhoto.css('opacity', '0.0').addClass('current').animate( { 'opacity': '1.0' }, 1000,
