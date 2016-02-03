aea8ba57f7c706f160efe2d89c853962fbe31a30
beckylehmann91 <beckylehmann91@gmail.com>
2016-02-03 10:26:25 -0600

    Play around with index page styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: 'logout'
+  # get '/about' => ''
 end
