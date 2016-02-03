ff3c0659e1f7b979bc7f17e8c6a41b06da4fee0e
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-03 16:42:38 -0600

    Added delete button to User's profile

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,7 +1,12 @@
 module PostHelper
 
-def authenticated?
-  session[:user_id] != nil
-end
+  def authenticated?
+    session[:user_id] != nil
+  end
+
+  # checks to see if content is belong's to current signed in user
+  def users_content?(post)
+    session[:user_id] == post.user_id
+  end
 
 end
